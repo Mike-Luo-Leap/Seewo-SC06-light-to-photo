b85b2ba9c8028d9d5624998cbd46f80a632e40a8
Mike_L <mikew2011@sina.com>
2025-02-16 21:55:01 +0800

    Update main.py增加了对焦模糊检测

--- main.py
+++ main.py
@@ -5,11 +5,19 @@ import PIL.Image
 import sys
 import pystray
 import threading
+import numpy as np
+import cv2
 
 # 加载图标
 image = PIL.Image.open("icon.png")
 
 stop_all = threading.Event()
+
+def captureScreen(region = None):
+    return cv2.cvtColor(np.array(pag.screenshot(region=region)), cv2.COLOR_RGB2BGR)
+
+def detectClarity(image):
+    return cv2.Laplacian(cv2.cvtColor(image, cv2.COLOR_RGB2BGR), cv2.CV_64F).var()
 
 def tray_icon_thread():
     def on_clicked():
@@ -24,15 +32,13 @@ def monitoring_thread():
     while not stop_all.is_set():
         if "希沃视频展台" == pgw.getActiveWindowTitle():
             time.sleep(3)
-            photoPosX, photoPosY = pag.locateCenterOnScreen("photo.png", confidence=0.8)
+            photoPos = pag.locateCenterOnScreen("photo.png", confidence=0.8)
             while True:
                 time.sleep(1)
-                try:
-                    lightPosX, lightPosY = pag.locateCenterOnScreen("lighted.png", confidence=0.8)
-                    pag.leftClick(photoPosX, photoPosY)
-                    pag.leftClick(lightPosX, lightPosY)
-                except:
-                    pass
+                lightPos = pag.locateCenterOnScreen("lighted.png", confidence=0.8)
+                if lightPos is not None and detectClarity(captureScreen(760, 340, 400, 400)):
+                    pag.leftClick(photoPos)
+                    pag.leftClick(lightPos)
 
 tray_thread = threading.Thread(target=tray_icon_thread)
 monitoring_thread = threading.Thread(target=monitoring_thread)
